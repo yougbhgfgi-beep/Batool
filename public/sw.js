865c359f3b28fe54f127546898e7a4c9c69d914b
yougbhgfgi@gmail-beep <yougbhgfgi@gmail.com>
2026-07-02 02:29:11 +0300

    Fix PWA - add PNG icons, بتول name on icon, improved install button

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "love-cache-v1";
-const urls = ["/", "/manifest.json", "/icon.svg"];
+const CACHE = "love-cache-v2";
+const urls = ["/", "/manifest.json", "/icon.svg", "/icon-192x192.png", "/icon-512x512.png", "/og-image.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
