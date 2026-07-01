9ea74842330605a0161e664721047a53ffdb8196
yougbhgfgi@gmail-beep <yougbhgfgi@gmail.com>
2026-07-02 02:15:55 +0300

    Love website code - Abdalrahman ❤️ Batool

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE = "love-cache-v1";
+const urls = ["/", "/manifest.json", "/icon.svg"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(urls))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
