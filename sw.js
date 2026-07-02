341f396c7e557714a8aea15a289f8a6527bc278c
yougbhgfgi@gmail-beep <yougbhgfgi@gmail.com>
2026-07-02 03:42:28 +0300

    Clean install flow - direct install, no guides

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "love-cache-v2";
+const CACHE = "love-cache-v3";
 const urls = ["./", "./index.html", "./manifest.json", "./icon.svg", "./icon-192x192.png", "./icon-512x512.png", "./og-image.png"];
 
 self.addEventListener("install", (e) => {
